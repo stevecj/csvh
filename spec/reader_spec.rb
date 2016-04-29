2014c66942e65e2800e7a3336653763e1a6c4afb
Steve Jorgensen <stevej@stevej.name>
2016-04-28 22:04:12 -0700

    behaviors for a CSV with hard-coded headers

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -38,16 +38,30 @@ a2,b2,c2
   describe "an instance" do
     subject { described_class.new(csv) }
 
-    let(:csv){ CSV.new(
-      csv_source_data,
+    let(:csv){
+      CSV.new(csv_source_data, csv_options)
+    }
+
+    let(:csv_options) { {
       headers: :first_row,
       return_headers: true
-    ) }
+    } }
 
     describe '#headers' do
-      context "with a pristine underlying CSV instance" do
+      context "with a pristine, typical underlying CSV instance" do
         it "returns an array of the headers from the first row" do
           expect( subject.headers ).to eq( %w(A B C) )
+        end
+      end
+
+      context "with an underlying CSV instance that has hard-coded headers" do
+        let(:csv_options) { {
+          headers: %w(-A- -B- -C-),
+          return_headers: true
+        } }
+
+        it "returns an array of the hard-coded headers" do
+          expect( subject.headers ).to eq( %w(-A- -B- -C-) )
         end
       end
 
@@ -65,23 +79,52 @@ a2,b2,c2
     end
 
     describe "#each" do
-      it "calls the given block for each data row when given a block argument" do
-        got_data_rows = []
-        subject.each do |row|
-          got_data_rows << row
+      context "with a typical underlying CSV instance" do
+        it "calls the given block for each data row when given a block argument" do
+          got_data_rows = []
+          subject.each do |row|
+            got_data_rows << row
+          end
+          expect( got_data_rows ).to eq( [
+            CSV::Row.new( %w(A B C), %w(a1 b1 c1) ),
+            CSV::Row.new( %w(A B C), %w(a2 b2 c2) )
+          ] )
         end
-        expect( got_data_rows ).to eq( [
-          CSV::Row.new( %w(A B C), %w(a1 b1 c1) ),
-          CSV::Row.new( %w(A B C), %w(a2 b2 c2) )
-        ] )
-      end
-    end
 
-    it "returns an enumerator that returns all of the data rows" do
-      actual = subject.each
-      expect( actual.next ).to eq( CSV::Row.new( %w(A B C), %w(a1 b1 c1) ) )
-      expect( actual.next ).to eq( CSV::Row.new( %w(A B C), %w(a2 b2 c2) ) )
-      expect{ actual.next }.to raise_exception( StopIteration )
+        it "returns an enumerator that returns all of the data rows" do
+          actual = subject.each
+          expect( actual.next ).to eq( CSV::Row.new( %w(A B C), %w(a1 b1 c1) ) )
+          expect( actual.next ).to eq( CSV::Row.new( %w(A B C), %w(a2 b2 c2) ) )
+          expect{ actual.next }.to raise_exception( StopIteration )
+        end
+      end
+
+      context "with an underlying CSV instance that has hard-coded headers" do
+        let(:csv_options) { {
+          headers: %w(-A- -B- -C-),
+          return_headers: true
+        } }
+
+        it "calls the given block for each row (including the 1st) when given a block argument" do
+          got_data_rows = []
+          subject.each do |row|
+            got_data_rows << row
+          end
+          expect( got_data_rows ).to eq( [
+            CSV::Row.new( %w(-A- -B- -C-), %w(A B C) ),
+            CSV::Row.new( %w(-A- -B- -C-), %w(a1 b1 c1) ),
+            CSV::Row.new( %w(-A- -B- -C-), %w(a2 b2 c2) )
+          ] )
+        end
+
+        it "returns an enumerator that returns all of the rows (including the 1st)" do
+          actual = subject.each
+          expect( actual.next ).to eq( CSV::Row.new( %w(-A- -B- -C-), %w(A B C) ) )
+          expect( actual.next ).to eq( CSV::Row.new( %w(-A- -B- -C-), %w(a1 b1 c1) ) )
+          expect( actual.next ).to eq( CSV::Row.new( %w(-A- -B- -C-), %w(a2 b2 c2) ) )
+          expect{ actual.next }.to raise_exception( StopIteration )
+        end
+      end
     end
   end
 
